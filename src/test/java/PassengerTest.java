5798bcd17f9f6c5da856176a4b5aa6f8d13f7e44
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-08-28 10:36:09 +0100

    initial commit

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,18 @@
+public class PassengerTest {
+
+    private String name;
+    private int bags;
+
+    public PassengerTest(String name, int bags) {
+        this.name = name;
+        this.bags = bags;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBags() {
+        return bags;
+    }
+}
