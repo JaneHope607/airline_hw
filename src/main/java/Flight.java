8b7acf231deb1872788b3ff4cc87290dfcf4a188
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-08-28 11:03:16 +0100

    added PlaneTest and FlightTest

---
+++ src/main/java/Flight.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String airport;
+    private String departure;
+
+    public Flight(ArrayList<Passenger> passengers, Plane plane, String flightNumber, String destination, String airport, String departure) {
+        this.passengers = passengers;
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.airport = airport;
+        this.departure = departure;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getAirport() {
+        return airport;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+}
+
+
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,5 @@
+public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+}
